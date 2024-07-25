3a8269e4c5ea229db85ad54def4eeaab1844f893
WuorBhang <uhuribhang211@gmail.com>
2024-07-25 08:54:05 +0300

    updating task_4.sql files

--- task_4.sql
+++ task_4.sql
@@ -4,4 +4,4 @@ USE alx_book_store;
 #Full description of the table 'books'
 SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA 
 FROM INFORMATION_SCHEMA.COLUMNS 
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
+WHERE TABLE_SCHEMA = DATABASE(alx_book_store) AND TABLE_NAME = 'Books';
